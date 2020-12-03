10c01b8863262612ad61429356ea37ef0c95b19a
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-03 18:41:26 +0530

    db connected

---
+++ db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb://localhost:27017/projectIdeaSaver",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (!err) {
+      console.log("Mongo Connected --Project Idea Saver");
+    } else {
+      console.log("Error in Mongo" + JSON.stringify(err, undefined, 2));
+    }
+  }
+);
--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+
+require("./db");
+
 app.listen(5000, () => {
   console.log("Server running for MERN Project Idea Server ......");
 });
