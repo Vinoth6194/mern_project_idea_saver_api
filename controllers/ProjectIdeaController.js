30bf09e9f4e392c81273f7dcbbc505e4a509a4ed
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-04 13:01:05 +0530

    del api

--- controllers/ProjectIdeaController.js
+++ controllers/ProjectIdeaController.js
@@ -57,4 +57,16 @@ router.put("/:id", (req, res) => {
   );
 });
 
+router.delete("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id))
+    return res.status(400).send("No record with given id : " + req.params.id);
+
+  ProjectIdeaModel.findByIdAndRemove(req.params.id, (err, docs) => {
+    if (!err) res.send(docs);
+    else
+      console.log(
+        "Error while deleting a record : " + JSON.stringify(err, undefined, 2)
+      );
+  });
+});
 module.exports = router;
