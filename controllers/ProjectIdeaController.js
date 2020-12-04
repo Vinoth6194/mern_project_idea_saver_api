46b91828a9e6658838f9342d56556f65c5094e52
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-04 12:43:28 +0530

    post api

--- controllers/ProjectIdeaController.js
+++ controllers/ProjectIdeaController.js
@@ -14,4 +14,21 @@ router.get("/", (req, res) => {
     }
   });
 });
+
+router.post("/", (req, res) => {
+  var newIdea = new ProjectIdeaModel({
+    sno: req.body.sno,
+    projectName: req.body.projectName,
+    status: req.body.status,
+  });
+  newIdea.save((err, docs) => {
+    if (!err) {
+      res.send(docs);
+    } else {
+      console.log(
+        "Error in adding the record " + JSON.stringify(err, undefined, 2)
+      );
+    }
+  });
+});
 module.exports = router;
