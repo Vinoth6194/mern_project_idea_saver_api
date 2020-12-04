2b52d4e466d6f3636ff417910a0eee35a31fadef
Vinoth6194 <49114903+Vinoth6194@users.noreply.github.com>
2020-12-04 12:47:21 +0530

    put api

--- controllers/ProjectIdeaController.js
+++ controllers/ProjectIdeaController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const ProjectIdeaModel = require("../models/ProjectIdeas");
+const ObjectID = require("mongoose").Types.ObjectId;
 
 router.get("/", (req, res) => {
   //res.json({ message: "Welcome to Project Idea Saver using MERN." });
@@ -31,4 +32,29 @@ router.post("/", (req, res) => {
     }
   });
 });
+
 module.exports = router;
+
+router.put("/:id", (req, res) => {
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(400).send("No record with given id : " + req.params.id);
+  }
+  var updatedRecord = {
+    sno: req.body.sno,
+    projectName: req.body.projectName,
+    status: req.body.status,
+  };
+  ProjectIdeaModel.findByIdAndUpdate(
+    req.params.id,
+    { $set: updatedRecord },
+    { new: true },
+    (err, docs) => {
+      console.log(docs);
+      if (!err) res.send(docs);
+      else
+        console.log(
+          "Error while updating a record : " + JSON.stringify(err, undefined, 2)
+        );
+    }
+  );
+});
